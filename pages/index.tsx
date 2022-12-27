4bc016f32564c28c5ef597ec7643b3beb296ae10
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-12-27 12:48:01 -0300

    feat: add footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
@@ -80,7 +81,9 @@ const Home: NextPage = ({ exploreData, cardsData }: any) => {
           description="Wishlists curated by Airbnb."
           buttonText="Get Inspired"
         />
+
       </main>
+      <Footer />
     </div>
   )
 }
