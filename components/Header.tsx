ea322160752464a896b259885ce88e23869409c9
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-12-27 10:56:14 -0300

    feat: add menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image'
-import { SearchIcon } from '@heroicons/react/solid'
+import { 
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon,
+} from '@heroicons/react/solid'
 
 export default function Header(props: any) {
   return (
@@ -17,8 +23,14 @@ export default function Header(props: any) {
       <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
     </div>
 
-    {/* right */}
-    <div></div>
+    <div className='flex items-center space-x-4 justify-end text-gray-500'>
+      <p className='hidden md:inline cursor-pointer'>Become a host</p>
+      <GlobeAltIcon className='h-6 cursor-pointer' />
+      <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+        <MenuIcon className='h-6' />
+        <UserCircleIcon className='h-6' />
+      </div>
+    </div>
 
     </header>
   )
