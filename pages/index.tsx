76d26f8184ae01ab5faf084bd504d815aa698bd4
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-12-27 11:54:31 -0300

    feat: add smallCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,24 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import SmallCard from '../components/SmallCard'
 
-const Home: NextPage = () => {
+export async function getStaticProps(_: any) {
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then(res => res.json())
+  return {
+    props: {
+      exploreData: exploreData || []
+    }
+  }
+}
+
+export interface ExploreData {
+  img: string
+  location: string
+  distance: string
+}
+
+const Home: NextPage = ({ exploreData }: any) => {
   return (
     <div className="">
       <Head>
@@ -13,6 +29,30 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            { exploreData.map(
+              ({ img, distance, location }: ExploreData, index: number) => 
+                <SmallCard 
+                  key={index} 
+                  img={img} 
+                  distance={distance} 
+                  location={location}
+                /> 
+              ) 
+            }
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+        </section>
+
+      </main>
     </div>
   )
 }
