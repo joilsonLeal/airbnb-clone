e28b4abc6056de0e539a8b882f3c7cccd73b2b3d
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-12-27 10:19:32 -0300

    feat: add search bar on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,9 @@ export default function Header(props: any) {
         alt="airbnb logo" />
     </div>
 
-    <div>
-      <input type="text" placeholder='Start your search'/>
-      <SearchIcon className='h-8'/>
+    <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+      <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start your search'/>
+      <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
     </div>
 
     {/* right */}
