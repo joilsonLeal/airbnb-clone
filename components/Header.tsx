b8077a3b062d997cd155f3b8d62b9d261f8906e8
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-12-27 14:39:37 -0300

    feat: add date range picker

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,35 +3,72 @@ import {
   GlobeAltIcon,
   MenuIcon,
   UserCircleIcon,
-  UsersIcon,
   SearchIcon,
 } from '@heroicons/react/solid'
+import { useState } from 'react'
+
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+
+import { DateRangePicker } from 'react-date-range'
 
 export default function Header(props: any) {
+  const [search, setSearch] = useState('')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+  
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection'
+  }
+
+  const handleSelectDate = (ranges: any) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
   return (
     <header className='stick top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-    <div className='relative flex items-center h-10 cursor-pointer my-auto'>
-      <Image 
-        src="https://links.papareact.com/qd3" 
-        fill={true} 
-        style={{ objectFit: 'contain', objectPosition: 'left' }}
-        alt="airbnb logo" />
-    </div>
-
-    <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-      <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start your search'/>
-      <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
-    </div>
-
-    <div className='flex items-center space-x-4 justify-end text-gray-500'>
-      <p className='hidden md:inline cursor-pointer'>Become a host</p>
-      <GlobeAltIcon className='h-6 cursor-pointer' />
-      <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
-        <MenuIcon className='h-6' />
-        <UserCircleIcon className='h-6' />
+      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+        <Image 
+          src="https://links.papareact.com/qd3" 
+          fill={true} 
+          style={{ objectFit: 'contain', objectPosition: 'left' }}
+          alt="airbnb logo" 
+        />
       </div>
-    </div>
 
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder='Start your search'
+          type="text"
+          className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' 
+        />
+        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
+      </div>
+
+      <div className='flex items-center space-x-4 justify-end text-gray-500'>
+        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer' />
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <MenuIcon className='h-6' />
+          <UserCircleIcon className='h-6' />
+        </div>
+      </div>
+
+      { search && (
+        <div className='flex flex-col col-span-3 mx-auto'>
+          <DateRangePicker 
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelectDate}
+          />
+        </div>
+      ) }
     </header>
   )
 }
